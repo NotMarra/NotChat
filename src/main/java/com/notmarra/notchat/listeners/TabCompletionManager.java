f16b73842b00e83700487609c808033f9b491b2c
TheNoiselessNoise <iceman980000@gmail.com>
2025-03-17 21:05:09 +0100

    lol

--- src/main/java/com/notmarra/notchat/listeners/TabCompletionManager.java
+++ src/main/java/com/notmarra/notchat/listeners/TabCompletionManager.java
@@ -1,19 +1,12 @@
 package com.notmarra.notchat.listeners;
 
-import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandSendEvent;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import net.milkbowl.vault.permission.Permission;
 
 import com.notmarra.notchat.NotChat;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 public class TabCompletionManager implements Listener {
     
@@ -31,28 +24,31 @@ public class TabCompletionManager implements Listener {
      * @return true pokud byla inicializace úspěšná, false pokud není dostupný Vault
      */
     public boolean initialize() {
-        // Kontrola dostupnosti Vaultu
-        if (plugin.getServer().getPluginManager().getPlugin("Vault") != null) {
-            // Inicializace Vault permission API
-            RegisteredServiceProvider<Permission> rsp = 
-                plugin.getServer().getServicesManager().getRegistration(Permission.class);
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        return true;
+
+        // // Kontrola dostupnosti Vaultu
+        // if (plugin.getServer().getPluginManager().getPlugin("Vault") != null) {
+        //     // Inicializace Vault permission API
+        //     RegisteredServiceProvider<Permission> rsp = 
+        //         plugin.getServer().getServicesManager().getRegistration(Permission.class);
             
-            if (rsp != null) {
-                perms = rsp.getProvider();
-                vaultEnabled = true;
+        //     if (rsp != null) {
+        //         perms = rsp.getProvider();
+        //         vaultEnabled = true;
                 
-                // Registrace event listeneru
-                plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        //         // Registrace event listeneru
+        //         plugin.getServer().getPluginManager().registerEvents(this, plugin);
                 
-                return true;
-            } else {
-                plugin.getLogger().warning("Vault nalezen, ale nelze získat Permission API");
-            }
-        } else {
-            plugin.getLogger().warning("Vault nenalezen - filtrace tab-completion nebude aktivní");
-        }
+        //         return true;
+        //     } else {
+        //         plugin.getLogger().warning("Vault nalezen, ale nelze získat Permission API");
+        //     }
+        // } else {
+        //     plugin.getLogger().warning("Vault nenalezen - filtrace tab-completion nebude aktivní");
+        // }
         
-        return false;
+        // return false;
     }
     
     /**
@@ -61,35 +57,38 @@ public class TabCompletionManager implements Listener {
     public void shutdown() {
         HandlerList.unregisterAll(this);
     }
-    
+
     @EventHandler
-    public void onPlayerCommandSend(PlayerCommandSendEvent event) {
-        if (!vaultEnabled) return;
+    public void onPlayerCommandSendEvent(PlayerCommandSendEvent event) {
+        // if (!vaultEnabled) return;
+        event.getCommands().clear();
+
+        //     CommandSender player = event.getSender();
+    //     Collection<String> commands = event.getCompletions();
         
-        Player player = event.getPlayer();
-        Collection<String> commands = event.getCommands();
+    //     // Vytvoření kopie seznamu příkazů
+    //     List<String> allCommands = new ArrayList<>(commands);
         
-        // Vytvoření kopie seznamu příkazů
-        List<String> allCommands = new ArrayList<>(commands);
+    //     // Vymazání všech příkazů
+    //     commands.clear();
         
-        // Vymazání všech příkazů
-        commands.clear();
-        
-        // Filtrace příkazů podle oprávnění hráče
-        for (String commandName : allCommands) {
-            Command command = plugin.getServer().getCommandMap().getCommand(commandName);
+    //     // Filtrace příkazů podle oprávnění hráče
+    //     for (String commandName : allCommands) {
+    //         Command command = plugin.getServer().getCommandMap().getCommand(commandName);
             
-            if (command == null) {
-                // Pokud command není nalezen, přidáme ho zpět (pro jistotu)
-                commands.add(commandName);
-                continue;
-            }
+    //         // if (command == null) {
+    //         //     // Pokud command není nalezen, přidáme ho zpět (pro jistotu)
+    //         //     commands.add(commandName);
+    //         //     continue;
+    //         // }
             
-            String permission = command.getPermission();
-            if (permission == null || perms.has(player, permission)) {
-                // Hráč má oprávnění, přidáme příkaz zpět do tab-completion
-                commands.add(commandName);
-            }
-        }
+    //         String permission = command.getPermission();
+    //         // if (permission == null || perms.has(player, permission)) {
+    //         if (permission == null || !player.hasPermission(permission)) {
+    //             event.setCancelled(true);
+    //             // Hráč má oprávnění, přidáme příkaz zpět do tab-completion
+    //             // commands.add(commandName);
+    //         }
+    //     }
     }
 }
